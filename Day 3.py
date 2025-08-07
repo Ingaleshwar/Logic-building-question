142099ead6bb3ffa952b41bd72752567e30428a5
Janani Ingaleshwar <rohithingaleshwar.14@gmail.com>
2025-08-07 21:19:27 +0530

    Intial changes

---
+++ Day 3.py
@@ -0,0 +1,46 @@
+# 1> Check for Armstrong number - A number is called an Armstrong number 
+# if the sum of its own digits each raised to the power of the number of digits is equal to the number itself.
+
+num=int(input("Enter a number to check it's armstrong number or not: "))
+
+def arm_strong(num):
+    len_num = len(str(num))
+    n=num
+    sum=0
+    while(num>0):
+        digit=num%10
+        sum+= digit**len_num
+        num//=10
+    if(n==sum):
+        print("Yes the number is Armstrong number!")
+    else:
+        print("The number is not Armstrong number!")
+    
+arm_strong(num) 
+
+# 2> Check for Perfect number
+num=int(input("Enter a number to check it is perfect number: "))
+def perfect_num(num):
+    if(num<=0):
+        print("Only positive intergers!")
+        return None
+    sum=0
+    for i in range(1,num):
+        if(num%i==0):
+            sum+=i
+    return sum==num
+
+if (perfect_num(num)):
+    print("The number is perfect number!")
+else: 
+    print("The number is not a perfect number!")
+
+# 3> Calculate simple interest
+p=float(input("Enter the principle amount: "))
+r=float(input("Enter the rate of interest: "))
+t=float(input("Enter time/duration in years: "))
+def S_I(p,r,t):
+    return ((p*r*t)/100)
+print(S_I(p,r,t))
+
+# 4  
